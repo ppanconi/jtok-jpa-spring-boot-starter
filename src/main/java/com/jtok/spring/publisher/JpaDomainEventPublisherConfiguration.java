85cf1423192f479d5cf1c43e82f8108afd6f8bcf
panks <p.panconi@gmail.com>
2023-01-10 15:25:38 +0100

    temp

--- src/main/java/com/jtok/spring/publisher/JpaDomainEventPublisherConfiguration.java
+++ src/main/java/com/jtok/spring/publisher/JpaDomainEventPublisherConfiguration.java
@@ -4,11 +4,13 @@ import com.jtok.spring.domainevent.DomainEventProcessor;
 import com.jtok.spring.domainevent.DomainEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @EntityScan({"com.jtok.spring.domainevent"})
